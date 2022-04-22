cb2b58b27682b933d0c7be7f39363ccc1b23e0f8
Micah Cooper <mc@micahcooper.com>
2022-04-22 10:13:15 -0400

    Changes todayCount to newestCount

--- uga.edu/monitor-undelivered-emails.ps1
+++ uga.edu/monitor-undelivered-emails.ps1
@@ -18,13 +18,13 @@ $toAddress = @("address1","address2")
 
 #script variables
 $date = get-date
-$todaycount = (Get-ChildItem $undeliveredFolder -File | Measure-Object).count
+$newestCount = (Get-ChildItem $undeliveredFolder -File | Measure-Object).count
 
 #Check existence of the files used in the script.
 if ( !(Test-Path -path "$scriptFolder\last-count") )
 {
     New-Item -Path $scriptFolder -Name "last-count" -ItemType File
-	$todaycount | Out-File -filepath "$scriptFolder\last-count"
+	$newestCount | Out-File -filepath "$scriptFolder\last-count"
 }
 if ( !(Test-Path -path "$scriptFolder\history.log") )
 {
@@ -33,17 +33,14 @@ if ( !(Test-Path -path "$scriptFolder\history.log") )
 
 $lastcount = Get-Content "$scriptFolder\last-count"
 
-##check today's count of emails vs the last count. If the same, then print to log. If different, print to log, send notification email, and update lastcount.
-if ($lastcount -ne $todaycount)
+##check newest count of emails vs the last count. If the same, print to log. If different, print to log, send notification email, and update lastcount.
+if ($lastcount -ne $newestCount)
 {
 	Add-content -path "$scriptFolder\history.log" -value ("$date -- There was a change in number of emails.")
 
-    $emailSubject = "Compass undelivered emails increased by $($todaycount-$lastcount)"
-	$emailMessage = "<p>The number of undelivered emails on compass.uga.edu increased by $($todaycount-$lastcount).<p>"
-	
-	$User = $emailUser
-	$cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User,
-					(Get-Content -path $emailPassword |	ConvertTo-SecureString -asplaintext -force)
+	$emailSubject = "Compass undelivered emails increased by $($newestCount-$lastcount)"
+	$emailMessage = "<p>The number of undelivered emails on compass.uga.edu increased by $($newestCount-$lastcount).<p>"
+	$cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $emailUser, (Get-Content -path $emailPassword | ConvertTo-SecureString -asplaintext -force)
 
 	$MailServerParams = @{
 		From  = $fromAddress
@@ -54,13 +51,13 @@ if ($lastcount -ne $todaycount)
 		Body = $emailMessage
 		Credential = $cred
 		Port = $emailPort
-    }
-		
+	}
+
 	Send-MailMessage @MailServerParams
 
-	$todaycount | Out-File -filepath "$scriptFolder\last-count"
+	$newestCount | Out-File -filepath "$scriptFolder\last-count"
 }
-elseif ($lastcount -eq $todaycount)
+elseif ($lastcount -eq $newestCount)
 {
 	Add-content -path "$scriptFolder\history.log" -value ("$date -- No email changes.")
 }
